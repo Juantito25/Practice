c2db3d94e500e2e76fc4e689f3154eed5c208b2b
Juan Rivera <rivej25@gmail.com>
2018-02-24 10:48:08 -0500

    Initial practice upload
    This is the first upload including the stopwatch practice

---
+++ Stopwatach/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer stopwatch = new Timer();
+            Console.WriteLine("Welcome to the timer");
+            Console.WriteLine();
+
+            int question;
+
+            do
+            {
+                question = 100;
+
+                Console.WriteLine("Please hit '1' to start. Once the timer has started you can hit 2 to stop. If you are done, please hit 0.");
+                var input = Console.ReadLine();
+
+                try
+                {
+                    question = Convert.ToInt32(input);
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not an acceptable answer");
+                    
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("this is not an acceptable asnwer");
+                }
+                
+
+                
+                    switch (question)
+                    {
+                        case 1:
+                            stopwatch.StartTimer();
+                            break;
+                        case 2:
+                            stopwatch.StopTimer();
+                            break;
+                    case 0:
+                        break;
+
+
+                        default:
+                            Console.WriteLine("This is not a valid Answer");
+                            break;
+                    }
+                } while (question != 0);
+        }
+    }
+}
---
+++ Stopwatach/StopWatch/StopWatch/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StopWatch
+{
+    public class Timer
+    {
+        private DateTime _startTime;
+        private Boolean _isRunning;
+        private TimeSpan _stopTime;
+        public bool isRunning;
+
+        public void StartTimer()
+        {
+            
+            this._isRunning =  isRunning;
+            if (isRunning) 
+            {
+                Console.WriteLine("The timer already started.");
+                return;
+            }
+            isRunning = true;
+            _startTime = DateTime.Now;
+        }
+
+        public void StopTimer()
+        {
+
+            this._isRunning = isRunning;
+
+            if (isRunning != true)
+            {
+                Console.WriteLine("The timer has not started.");
+                return;
+            }
+
+            isRunning = false;
+            _stopTime = DateTime.Now - _startTime;
+            Console.WriteLine("The time that elaped was : {0}", _stopTime);
+        }
+    }
+}
